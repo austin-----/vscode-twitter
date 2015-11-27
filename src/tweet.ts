142aadc9a5fc6f4fa5abd6784e14581d5155853e
Austin <austiny.cn@gmail.com>
2015-11-27 04:09:08 -0800

    UI Tweaks

--- src/tweet.ts
+++ src/tweet.ts
@@ -2,7 +2,9 @@ var XRegExp = require('xregexp').XRegExp;
 var moment = require('moment');
 
 export default class Tweet {
+	id: string;
 	text: string;
+	userId: string;
 	userName: string;
 	userScreenName: string;
 	userImage: string;
@@ -14,27 +16,49 @@ export default class Tweet {
 	static endLine: string = '_____' + Tweet.lineFeed;
 	static quote: string = '>';
 	
+	tweetLink(): string {
+		return 'https://twitter.com/' + this.userScreenName + '/status/' + this.id;
+	}
+	
+	userLinkPrefix(): string {
+		return 'https://twitter.com/';
+	}
+	
+	userLink(): string {
+		return this.userLinkPrefix() + this.userScreenName;
+	}
+	
+	hashTagLinkPrefix(): string {
+		return 'https://twitter.com/hashtag/';
+	}
+	
+	hashTagLink(hashtag: string): string {
+		return this.hashTagLinkPrefix() + hashtag;
+	}
+	
 	toMarkdown(level: number = 0) : string {
 		const quote = new Array(level + 1).join(Tweet.quote);
 		var result = quote + this.formatUser(level) + Tweet.lineFeed + 
-			quote + Tweet.normalize(this.text) + Tweet.lineFeed;
+			quote + this.normalize(this.text) + Tweet.lineFeed;
 		if (this.quoted) {
 			result += this.quoted.toMarkdown(level + 1);
 		}
-		if (level == 0) {
-			if (this.media) {
-				this.media.forEach((value, index, array) => {
-					result += '![](' + value.media_url_https + ')' + Tweet.lineFeed;
-				});
-			}
+		
+		if (this.media) {
+			var size = (level == 0) ? ':small' : ':thumb';
+			this.media.forEach((value, index, array) => {
+				result += quote + '[![](' + value.media_url_https + size + ')](' + value.media_url_https + ':large)' + Tweet.lineFeed;
+			});
 		}
 		if (level == 0) result += Tweet.endLine;
 		return result;
 	}
 	
-	constructor(created: string, text: string, userName: string, userScreenName: string, userImage: string) {
+	constructor(id: string, created: string, text: string, userId: string, userName: string, userScreenName: string, userImage: string) {
+		this.id = id;
 		this.created = created;
 		this.text = text;
+		this.userId = userId;
 		this.userName = userName;
 		this.userScreenName = userScreenName;
 		this.userImage = userImage;
@@ -45,10 +69,11 @@ export default class Tweet {
 		if (level == 0) {
 			result += '![](' + this.userImage + ') ';
 		}
-		result += Tweet.bold(this.userName) + ' [@' + this.userScreenName.replace('_', '\uFF3F') + '](#)';
+		result += Tweet.bold(this.userName) + ' [@' + this.userScreenName.replace('_', '\uFF3F') + '](' + this.userLink() + ')';
 		if (level == 0) {
 			result += ' \u2022 ' + moment(this.created.replace(/( +)/, ' UTC$1')).fromNow();
 		}
+		result += ' [(Detail)](' + this.tweetLink() + ')'; 
 		return result;
 	}
 	
@@ -60,14 +85,15 @@ export default class Tweet {
 		return '#' + text + '\r\n\r\n';
 	}
 	
-	static normalize(text: string) : string {
-		var hashtag = new XRegExp('(#[\\pL\\d_]+)', 'g');
-		var user = new XRegExp('(@[\\pL\\d_]+)', 'g');
-		return text.replace(hashtag, '[$1](#)').replace(user, '[$1](#)').replace('_', '\uFF3F');
+	normalize(text: string) : string {
+		var hashtag = new XRegExp('#([\\pL\\d_]+)', 'g');
+		var user = new XRegExp('@([\\pL\\d_]+)', 'g');
+		var rt = new XRegExp('^RT ', 'g');
+		return text.replace(hashtag, '[#$1](' + this.hashTagLinkPrefix() + '$1)').replace(user, '[@$1](' + this.userLinkPrefix() + '$1)').replace('_', '\uFF3F').replace(rt, '**RT** ');
 	}
 	
 	static fromJson(json: any) : Tweet {
-		var tweet = new Tweet (json.created_at, json.text, json.user.name, json.user.screen_name, json.user.profile_image_url_https);
+		var tweet = new Tweet (json.id_str, json.created_at, json.text, json.user.id_str, json.user.name, json.user.screen_name, json.user.profile_image_url_https);
 		if (json.quoted_status) {
 			tweet.quoted = Tweet.fromJson(json.quoted_status);
 		}
