a5e8b990ef9f49602bc305db57f94efd8c7472b1
Austin <austiny.cn@gmail.com>
2019-03-23 08:52:21 -0700

    bundle with webpack

--- src/views/webview.ts
+++ src/views/webview.ts
@@ -6,19 +6,19 @@ var uniqid = require('uniqid');
 
 export default class WebView {
     static GetWebViewContent(context: vscode.ExtensionContext, type: TimelineType, data: any): Thenable<string> {
-        return WebView.RenderWebContent(context, 'out/src/views/timeline.ejs', data);
+        return WebView.RenderWebContent(context, 'dist/views/timeline.ejs', data);
     }
 
     static GetRetweetLink(context: vscode.ExtensionContext, data: any): Thenable<string> {
-        return WebView.RenderWebContent(context, 'out/src/views/retweetlink.ejs', data);
+        return WebView.RenderWebContent(context, 'dist/views/retweetlink.ejs', data);
     }
 
     static GetLikeLink(context: vscode.ExtensionContext, data: any): Thenable<string> {
-        return WebView.RenderWebContent(context, 'out/src/views/likelink.ejs', data);
+        return WebView.RenderWebContent(context, 'dist/views/likelink.ejs', data);
     }
 
     static GetFollowLink(context: vscode.ExtensionContext, data: any): Thenable<string> {
-        return WebView.RenderWebContent(context, 'out/src/views/followlink.ejs', data);
+        return WebView.RenderWebContent(context, 'dist/views/followlink.ejs', data);
     }
 
     private static RenderWebContent(context: vscode.ExtensionContext, path: string, data: any): Thenable<string> {
